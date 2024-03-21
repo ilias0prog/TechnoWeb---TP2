ebe935a2c12f5fda20ad1a0697f61cc4ee0a73b3
ilias0prog <iliasess12@gmail.com>
2024-03-21 22:06:43 +0100

    Projet terminé

--- Code du prof/todo_list/my_app/app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-from my_app.routes.tasks import router as task_router
-
-
-app = FastAPI(title="My Todo List")
-app.mount("/static", StaticFiles(directory="static"))
-app.include_router(task_router)
